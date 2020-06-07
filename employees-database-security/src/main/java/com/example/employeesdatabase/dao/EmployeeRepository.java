46f5cb123645cf4231bf79b622317f0dc455a633
Your Name <you@example.com>
2020-06-07 21:30:08 +0200

    first commit

---
+++ employees-database-security/src/main/java/com/example/employeesdatabase/dao/EmployeeRepository.java
@@ -0,0 +1,15 @@
+package com.example.employeesdatabase.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.employeesdatabase.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	public List<Employee> findAllByOrderByLastNameAsc();
+	
+	public List<Employee> findByFirstNameContainsOrLastNameContainsAllIgnoreCase(String name, String lName);
+	
+}
